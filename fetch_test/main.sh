ae31a66911aadcb7593db10db4d2c4bd694ca0af
supermariobros374 <supermariobros374@gmail.com>
2017-08-16 12:51:24 +0800

    change main.sh 10th - Major update

--- fetch_test/main.sh
+++ fetch_test/main.sh
@@ -5,27 +5,4 @@ cat source.html | sed 's/spanid/span id/g'  |  tidy -imq --show-errors 0 | grep 
 cat source.html | sed 's/spanid/span id/g'  |  tidy -imq --show-errors 0 | sed 's/、/ /g' | grep '".*">[0-9].*<' | grep 'span' | sed 's/class=\".*\"//g;s/\".*\"//g;s/<.*>//g;s/>//g;4,$ s/[0-9]//g;1,2 s/\ /:/g;4,$ s/\ /:/g;/:/d;s/^[\ ]*/:/g;s/:/頭獎：/;s/ /;/g'> fetch_head
 cat ./fetch_head >> fetch_exp_head
 mv fetch_exp_head fetch_result.txt
-read -p "輸入發票末三碼：" last_three
-echo "輸入的碼是：" ${last_three}
-read -p "(y/n)" confirm
-grep_res=$(grep "${last_three}" fetch_result.txt)
-if [[ ${confirm} == 'y' ]] ; then
-  if [[ ${grep_res} ]] ; then
-    if [[ $(echo ${grep_res} | sed 's/[0-9].*//;s/：//') ==  "增開陸獎" ]] ; then
-      echo "增開陸獎"
-      exit 0
-    else
-      read -p "輸入全部號碼：" all_code
-      echo "輸入的碼是：" ${all_code}
-      read -p "(y/n)" sec_confirm
-      if [[ ${sec_confirm} == 'y' ]] ; then
-        exit 0
-      else
-        exit 0
-      fi
-    fi
-  fi
-else
-  echo "Exit..."
-  exit 0
-fi
+./receipt
