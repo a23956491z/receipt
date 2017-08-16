c935960d90bb84aee40f6322c5f701182b514e2c
supermariobros374 <supermariobros374@gmail.com>
2017-08-16 10:46:45 +0800

    1st commit _ fetch

--- fetch_test/fetch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -Ls http://invoice.etax.nat.gov.tw/ | sed 's/spanid/span id/g'  |  tidy -imq --show-errors 0 | grep 'class=".*">[0-9]*<' | sed 's/class=\".*\"//g' | sed 's/<h.*>//g' | sed 's/[^0-9]//g'
